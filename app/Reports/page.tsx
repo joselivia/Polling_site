d85eec35a77862ba4e343aa4ef38343a5b6bbaf5
Joselivia <josephmutunga39@gmail.com>
2025-07-31 10:19:40 +0300

    sdf

--- app/Reports/page.tsx
+++ app/Reports/page.tsx
@@ -38,7 +38,12 @@ export default function HomePage() {
                     className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition"
                   >
                     Logout
-                  </button>
+                  </button>  <a
+                    href="/Login/update-admin"
+                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+                  >
+                    Update profile
+                  </a>
                   <a
                     href="/dummyCreatePoll/createpoll"
                     className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
